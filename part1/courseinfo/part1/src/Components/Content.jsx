6d9f9638c3886fe7324c126bbf2a87d24424b7bd
Lan Yang Villavicencio <leelanzing@hotmail.com>
2024-10-28 15:57:15 -0300

    1.3: Información del Curso, paso 3

--- part1/courseinfo/part1/src/Components/Content.jsx
+++ part1/courseinfo/part1/src/Components/Content.jsx
@@ -3,9 +3,9 @@ const Content = ({ part1, exercises1, part2, exercises2, part3, exercises3 }) =>
     return (
 
         <div>
-            <Part name={part1} exercises={exercises1} />
-            <Part name={part2} exercises={exercises2} />
-            <Part name={part3} exercises={exercises3} />
+            <Part name={part1.name} exercises={part1.exercises} />
+            <Part name={part2.name} exercises={part2.exercises} />
+            <Part name={part3.name} exercises={part2.exercises} />
         </div>
 
 
--- part1/courseinfo/part1/src/Components/Total.jsx
+++ part1/courseinfo/part1/src/Components/Total.jsx
@@ -1,5 +1,5 @@
-const Total = ({ exercises1, exercises2, exercises3 }) => {
-    return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-}
+const Total = ({ part1, part2, part3 }) => {
+    return <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
+  }
 
 export default Total
