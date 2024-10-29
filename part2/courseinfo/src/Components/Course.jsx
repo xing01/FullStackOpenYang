51d3220297151d7d0e86961ee09f7547d7836ea6
Lan Yang Villavicencio <leelanzing@hotmail.com>
2024-10-29 00:50:40 -0300

    2.3*: Información del curso paso 8

--- part2/courseinfo/src/Components/Course.jsx
+++ part2/courseinfo/src/Components/Course.jsx
@@ -4,7 +4,12 @@ import Content from './Content';
 
 const Course = ({ course }) => {
 
-    const totalExercises = course.parts.reduce((sum, part) => sum + part.exercises, 0)
+    const totalExercises = course.parts.reduce((sum, part) => {
+        console.log('suma:', sum)
+        console.log('part:', part)
+        return sum + part.exercises
+      }, 0)
+
     return (
         <div>
             <Header title={course.name} />
